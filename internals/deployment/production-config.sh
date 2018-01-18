9f79053a2bf1e3ae376bb8b48ad952f35913a80a
Tom Grant <tom.grant@tophatmonocle.com>
2018-01-18 17:46:21 -0500

    Add new certificate

--- internals/deployment/production-config.sh
+++ internals/deployment/production-config.sh
@@ -2,7 +2,7 @@
 
 APP_NAME="sunburst-fe-boilerplate"
 BUCKET_NAME="sunburst-fe-boilerplate-production"
-CERTIFICATE_ARN=""
+CERTIFICATE_ARN="arn:aws:acm:us-east-1:820704410829:certificate/c792ea70-ae68-45e5-b372-933e97891963"  # *.sunburst.io
 DOMAIN_NAME="boilerplate.sunburst.io"
 HOSTED_ZONE_NAME="sunburst.io."
 STACK_NAME="sunburst-fe-boilerplate-production-stack"
